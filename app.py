b084adb1d9bf3ffd320a829a0411d0b40d8b5346
Rushil Kapadia <rushilkapadia@macbook-pro.lan>
2021-05-10 14:30:55 -0700

    actual website

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from main import pun_from_word
+
+app = Flask(__name__)
+
+ret = [" ", " "]
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+	if request.method == "POST":
+	    print(request.form)
+	    print(request.form.get("amount"))
+	    wrd = request.form.get("amount")
+	    if not (wrd == ' ' or wrd == ''):
+	    	ret[0] = pun_from_word(wrd)
+	print(ret[0] == " ")
+	if not ret[0] == " ":
+		ret[1] = "Generated Pun"
+	else:
+		ret[1] = ""
+
+	return render_template("form.html", entries=ret)
+
+# @app.route("/pun")
+# def show_pun():
+# 	return render_template("pun.html", entries=pun)
